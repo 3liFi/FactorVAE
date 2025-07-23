372f41096a718b61df935406ddad45024060e3d0
fynn-bckr <fbecker879@icloud.com>
2025-07-23 14:37:17 +0200

    docs: Add docs to vae.py

--- vae.py
+++ vae.py
@@ -5,29 +5,54 @@ import torch.nn.functional as f
 import torch
 import math
 
-# FEATURE_MAP_H = 7
-# FEATURE_MAP_W = 7
 LATENT_DIM = 8
 SOURCE_IMAGE_DIM = 28
 
-
 class HyperParams(NamedTuple):
+    """
+    A class used to store hyperparameters of the VAE. They will be used across the entire network.
+
+    Attributes:
+            kernel_size (int): Size of the convolution kernel. Default is 3.
+            stride (int): The stride of the convolution. Default is 2.
+            padding (int): The padding size of the convolution. Default is 1.
+    """
     kernel_size: int = 3
     stride: int = 2
     padding: int = 1
 
 
-"""
-This function ignores dilation for now.
-"""
-
-
 def calc_fmap_size(source_dim: int, kernel_size: int, stride: int, padding: int) -> int:
+    """
+    Calculates the feature map size given hyperparameters used in the network. Dilation is assumed to be 1 (i.e. ignored).
+
+    Args:
+        source_dim (int): The size of the previous layer (can also be input size), assumes square layers, so height OR width is fine
+        kernel_size (int): The size of the convolution kernel
+        stride (int): The stride of the convolution
+        padding (int): The padding size of the convolution
+
+    Returns:
+        int: The size of the feature map resulting from the convolution
+    """
     return math.floor((source_dim + 2 * padding - (kernel_size - 1) - 1) / stride + 1)
 
 
 def calc_outer_padding_based_on_desired_output_dims(source_dim: int, target_dim: int, kernel_size: int, stride: int,
                                                     padding: int) -> int:
+    """
+    Calculates the outer padding that a ConvTranspose2d layer must use to arrive at the provided target_dim.
+
+    Args:
+        source_dim (int): The size of the previous layer (can also be input size), assumes square layers, so height OR width is fine
+        target_dim (int): The size that the output of the current convolution should have, assumes square layers, so height OR width is fine
+        kernel_size (int): The size of the convolution kernel
+        stride (int): The stride of the convolution
+        padding (int): The padding size of the convolution
+
+    Returns:
+        int: The outer padding needed to arrive at the target_dim
+    """
     curr_out_dim = (source_dim - 1) * stride - 2 * padding + kernel_size  # + outer_padding
     return target_dim - curr_out_dim
 
@@ -39,18 +64,21 @@ class Encoder(nn.Module):
 
         self.params = params
 
+        """
+        Convolution part of the encoder. When using 28x28 images and default hyperparameters, it will output 5x5 feature maps.
+        We use a BatchNorm2d layer just before the end to normalize values and ensure training stability.
+        """
         self.conv = nn.Sequential(
             nn.Conv2d(1, 128, params.kernel_size, params.stride, params.padding, padding_mode='replicate'),
-            #nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=False),
             nn.Conv2d(128, 256, params.kernel_size, params.stride, params.padding, padding_mode='replicate'),
-            #nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=False),
             nn.Conv2d(256, 512, params.kernel_size, 1, params.padding, padding_mode='replicate'),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=False),
         )
 
+        # Dense network responsible for calculating mu.
         self.fc_mu = nn.Sequential(
             nn.Linear(512 * feature_map_dim * feature_map_dim, 256),
             nn.LeakyReLU(0.2, inplace=False),
@@ -60,6 +88,8 @@ class Encoder(nn.Module):
             nn.LeakyReLU(0.2, inplace=False),
             nn.Linear(64, latent_dim),
         )
+
+        # Dense network responsible for calculating the logvar.
         self.fc_logvar = nn.Sequential(
             nn.Linear(512 * feature_map_dim * feature_map_dim, 256),
             nn.LeakyReLU(0.2, inplace=False),
@@ -68,18 +98,17 @@ class Encoder(nn.Module):
             nn.Linear(128, 64),
             nn.LeakyReLU(0.2, inplace=False),
             nn.Linear(64, latent_dim),
-            #nn.Tanh(),
         )
 
     def forward(self, x):
-        # extract feature maps
+        # Apply convolutions to extract feature maps from input
         x = self.conv(x)
 
         # flatten feature maps from (batch_size, feature_map_amount, feature_map_h, feature_map_w) to (batch_size, rest)
         x = x.view(x.size(0), -1)
 
         # calculate mu and standard deviation
-        return self.fc_mu(x), self.fc_logvar(x)# * 4
+        return self.fc_mu(x), self.fc_logvar(x)
 
 
 class Decoder(nn.Module):
@@ -92,8 +121,6 @@ class Decoder(nn.Module):
 
         # latent space -> feature map
         self.fc = nn.Sequential(
-            #nn.Linear(latent_dim, 64),
-            #nn.LeakyReLU(0.2, inplace=False),
             nn.Linear(latent_dim, 128),
             nn.LeakyReLU(0.2, inplace=False),
             nn.Linear(128, 256),
@@ -102,22 +129,13 @@ class Decoder(nn.Module):
             nn.LeakyReLU(0.2, inplace=False),
             nn.Linear(512, 1024 * self.feature_map_dim * self.feature_map_dim),
             nn.LeakyReLU(0.2, inplace=False),
-            #nn.Linear(1024, 2048 * self.feature_map_dim * self.feature_map_dim),
-            #nn.LeakyReLU(0.2, inplace=False),
         )
 
-        # todo calculate outer padding based on desired output size
-        # 'undo' convolution from encoder
+        """
+        ConvTranspose2d layers paired with normalization through BatchNorm2d will convert our feature maps back to an image.
+        When using 28x28 images and the default hyperparameters, it will output a 28x28 image and accept 5x5 feature maps.
+        """
         self.deconv = nn.Sequential(
-            # this layer should keep feature_map_dim resolution, so stride needs to be adjusted
-            #nn.ConvTranspose2d(
-            #    2048, 1024, params.kernel_size, stride=1, padding=params.padding,
-            #    output_padding=calc_outer_padding_based_on_desired_output_dims(
-            #        feature_map_dim, feature_map_dim, params.kernel_size, 1, params.padding
-            #    )
-            #),
-            #nn.BatchNorm2d(1024),
-            #nn.LeakyReLU(0.2, inplace=False),
             # this layer should keep feature_map_dim resolution, so stride needs to be adjusted
             nn.ConvTranspose2d(
                 1024, 512, params.kernel_size, stride=1, padding=params.padding,
@@ -155,6 +173,7 @@ class Decoder(nn.Module):
     def forward(self, z):
         # convert to feature map
         x = self.fc(z)
+
         # unflatten data
         x = x.view(-1, 1024, self.feature_map_dim, self.feature_map_dim)
 
@@ -211,33 +230,21 @@ class VAE(nn.Module):
 
         # offset mu by small value within standard deviation
         return mu + eps * std
-        # todo re-enable random picks eventually
-        #return mu
 
 
 def vae_loss(recon_x, x, mu, logvar, total_correlation, current_epoch):
-    # print("loss 1: ", x.max)
-    #recon_loss = f.binary_cross_entropy(recon_x, x, reduction='sum')
-    # recon_loss = f.binary_cross_entropy_with_logits(recon_x, x, reduction='sum')
+    # Use MSE to calculate reconstruction loss, in the future some kind of perception loss may improve the quality
     recon_loss = f.mse_loss(recon_x, x, reduction="sum")
-    #recon_loss = f.l1_loss(recon_x, x, reduction="sum")
-    #logvar = torch.clamp(logvar, min=-10, max=10)
+
+    # Calculate KLD
     kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-    # for 30 epochs, this goes from 0.002 to 0.06
-    #beta = min(1, (current_epoch - 10) / 400)
-    #if current_epoch <= 10:
-    #  beta = 0.001
-    #beta = 0.5
+    # Use beta annealing from 0.03 to 0.7 over 30 epochs
     target_beta = 0.7
     beta = min(target_beta, max(0.001, (current_epoch / 30) * target_beta))
-    #beta = min(1.0, current_epoch / 200 * 0.05)
-    #loss = recon_loss + beta * kld
 
-    gamma = 0
+    # TODO: reevaluate gamma
+    gamma = 15
 
-    #if current_epoch < 5:
-        #gamma = 0.0
     loss = recon_loss + kld * beta + gamma * total_correlation
-    # print("loss 2: ", loss)
     return loss, recon_loss
